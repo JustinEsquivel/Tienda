9e0ae25937c30501ec9181e4cfa08effa2f1307d
justi <justi@JUSTIN-PC>
2025-03-27 20:59:59 -0600

    Semana 11

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -2,6 +2,7 @@ package com.tienda.domain;
 import jakarta.persistence.*; 
 import java.io.Serializable; 
 import lombok.Data;
+import java.util.List;
 @Data
 @Entity 
 @Table(name="Categoria")/*Tabla a la que va a estar asociados los datos*/
@@ -16,6 +17,10 @@ public class Categoria implements Serializable /*Encargado de habilitar el auto 
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
+    @OneToMany
+    @JoinColumn(name = "id_categoria", updatable = false)
+    List<Producto> productos;
+    
     public Categoria() 
     {
     }
